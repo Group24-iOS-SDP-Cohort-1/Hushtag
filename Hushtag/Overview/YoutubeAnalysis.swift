37626cfc024a45f0306db921855590ca45f00e30
Aarushi Deshmukh <aarushideshmukh8@gmail.com>
2025-11-20 10:11:00 +0530

    MVC architecture

--- Hushtag/Overview/YoutubeAnalysis.swift
+++ Hushtag/Overview/YoutubeAnalysis.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class YoutubeAnalysis: UIViewController {
 
+    @IBOutlet weak var AnalysisCollectionView: UICollectionView!
+    let values = ["6%", "3%", "24K"]
+    let categories = ["Views", "Likes", "Subscribers"]
+    
     var someProperty: String?
         
         override func viewDidLoad() {
             super.viewDidLoad()
-            print("Received: \(someProperty ?? "nil")")
+            AnalysisCollectionView.delegate = self
+            AnalysisCollectionView.dataSource = self
         }
 
+}
+
+extension YoutubeAnalysis: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return values.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = AnalysisCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! AnalysisCollectionViewCell
+        
+        cell.ValueLabel.text = values[indexPath.row]
+        cell.CategoryLabel.text = categories[indexPath.row]
+        return cell
+    }
+    
+    
 }
