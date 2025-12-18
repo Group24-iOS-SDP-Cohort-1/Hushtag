bc42726b75681d642ade3729d69b0b1dc280173f
Gargi <gargipratapwar@gmail.com>
2025-12-18 16:22:16 +0530

    images and all

--- Hushtag/View/Ideate/IdeaCells.swift
+++ Hushtag/View/Ideate/IdeaCells.swift
@@ -19,6 +19,9 @@ class IdeaCells: UICollectionViewCell {
 
     @IBOutlet weak var engagementLabel: UILabel!
 
+    
+    @IBOutlet weak var engagementImage: UIImageView!
+
     var idea: Idea?
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -34,8 +37,6 @@ class IdeaCells: UICollectionViewCell {
     func configure(idea: Idea) {
         ideaTitle.text = idea.title
         configureHashtags(idea.hashtag)
-        //configureEngagement(idea.engagementTag)
-
     }
 
     private func configureHashtags(_ hashtags: [String]) {
@@ -59,52 +60,6 @@ class IdeaCells: UICollectionViewCell {
             badgeStack.addArrangedSubview(badge)
         }
     }
-
-//    private func configureEngagement(_ engagementTag: String) {
-//
-//        if engagementTag.lowercased().contains("trending") {
-//            engagementLabel.text = "🔥 Trending"
-//            engagementLabel.textColor = .systemRed
-//        } else if engagementTag.lowercased().contains("enagaging")  {
-//            engagementLabel.text = "⚡ Engaging"
-//            engagementLabel.textColor = .label
-//        } else {
-//            engagementLabel.text = "⚡ Trending"
-//            engagementLabel.textColor = .label
-//        }
-//    }
-
-//    private func configureEngagementTag(_ engagementTag: String) {
-//        let symbolName: String
-//        let text: String
-//        let color: UIColor
-//
-//        if engagementTag.lowercased().contains("trending") {
-//            symbolName = "flame.fill"
-//            text = " Trending"
-//            color = .systemRed
-//        } else if engagementTag.lowercased().contains("engaging") {
-//            symbolName = "bolt.fill"
-//            text = " Engaging"
-//            color = UIColor.systemYellow
-//        } else {
-//            symbolName = "sparkles"
-//            text = " Trending"
-//            color = .label
-//        }
-//
-//        let attachment = NSTextAttachment()
-//        if let image = UIImage(systemName: symbolName)?.withTintColor(color, renderingMode: .alwaysOriginal) {
-//            attachment.image = image
-//            // optional: adjust size
-//            attachment.bounds = CGRect(x: 0, y: -2, width: 16, height: 16)
-//        }
-//
-//        let attributedString = NSMutableAttributedString(attachment: attachment)
-//        attributedString.append(NSAttributedString(string: text, attributes: [.foregroundColor: color, .font: UIFont.systemFont(ofSize: 14, weight: .semibold)]))
-//
-//        engagementLabel.attributedText = attributedString
-//    }
 
 
 
