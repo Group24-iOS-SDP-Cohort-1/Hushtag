3577623de951624984eb72fa9fa84ca2977b34be
Aarushi Deshmukh <aarushideshmukh8@gmail.com>
2026-01-21 14:22:52 +0530

    tab bar changes

--- Hushtag/TabBarController.swift
+++ Hushtag/TabBarController.swift
@@ -14,14 +14,14 @@ class MainTabBarController: UITabBarController {
             viewControllers[0].tabBarItem.title = "Ideate"
             viewControllers[0].tabBarItem.image = UIImage(systemName: "sparkles.2")
             
-            viewControllers[1].tabBarItem.title = "Deals"
-            viewControllers[1].tabBarItem.image = UIImage(systemName: "list.bullet")
-
-            viewControllers[2].tabBarItem.title = "Schedule"
-            viewControllers[2].tabBarItem.image = UIImage(systemName: "calendar")
+            viewControllers[1].tabBarItem.title = "Analysis"
+            viewControllers[1].tabBarItem.image = UIImage(systemName: "chart.bar.xaxis")
             
-            viewControllers[3].tabBarItem.title = "Analysis"
-            viewControllers[3].tabBarItem.image = UIImage(systemName: "chart.bar.xaxis")
+            viewControllers[2].tabBarItem.title = "Deals"
+            viewControllers[2].tabBarItem.image = UIImage(systemName: "list.bullet")
+
+            viewControllers[3].tabBarItem.title = "Schedule"
+            viewControllers[3].tabBarItem.image = UIImage(systemName: "calendar")
         }
     }
 }
