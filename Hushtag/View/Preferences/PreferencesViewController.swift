c9d76e1cd6d1e63295c9a8a65833315323e6469b
DhruavMathur <dhruavmathur@gmail.com>
2026-01-28 15:30:49 +0530

    Preferences attached to login and sign up

--- Hushtag/View/Preferences/PreferencesViewController.swift
+++ Hushtag/View/Preferences/PreferencesViewController.swift
@@ -42,7 +42,7 @@ class PreferencesViewController: UIViewController {
     //NEW
     private var completedStates: [Bool] = []
     
-    
+    //Dictionary of all the preferences
     private var selectedOptions: [String: [String]] = [
         "Niche": [],
         "Content Goals": [],
