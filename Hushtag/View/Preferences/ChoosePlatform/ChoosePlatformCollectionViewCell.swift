1fde7067505b28c85cce9ba9e6a7eb4f547167a8
DhruavMathur <dhruavmathur@gmail.com>
2026-03-24 14:16:47 +0530

    Only kept preferences for niche and added a new preferences card of choose platform.

---
+++ Hushtag/View/Preferences/ChoosePlatform/ChoosePlatformCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ChoosePlatformCollectionViewCell.swift
+//  Hushtag
+//
+//  Created by SDC-USER on 24/03/26.
+//
+
+import UIKit
+
+class ChoosePlatformCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
